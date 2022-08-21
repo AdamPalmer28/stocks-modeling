8c45dcba8b90c05751a8ba43387acf580e78532e
AdamPalmer28 <93833080+AdamPalmer28@users.noreply.github.com>
2022-08-21 13:50:00 +0100

    Class handlers

---
+++ DataClass.py
@@ -0,0 +1,15 @@
+"""
+
+"""
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+class StockData:
+    "Class for data handelling given a stock ticker"
+    def __init__(self):
+
+        pass
+
+
---
+++ strategy_eval.py
@@ -0,0 +1,9 @@
+"""
+Evaluates the stock strategy - presenting various stats about how the stock performs
+"""
+
+class strategy:
+    "Strategy evaluation for a given stock dataset"
+    def __init__(self, data):
+
+        pass
